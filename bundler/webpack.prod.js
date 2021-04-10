95e99a8a9f6fb670c11d2db81ec139f32ff9146f
Harvey Ngo <harveydngo@gmail.com>
2021-04-09 23:14:36 -0700

    BEP - Let's Get It Started

---
+++ bundler/webpack.prod.js
@@ -0,0 +1,37 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const newCssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = merge(
+    common,
+    {
+        mode: 'production',
+        devtool: 'source-map',
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css',
+            }),
+            // new CopyPlugin({
+            //     patterns: [
+            //       { from: '_redirects' }
+            //     ]
+            // })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader']
+                }
+            ]
+        },
+        optimization: {
+            minimizer: [
+                new newCssMinimizerPlugin()
+            ]
+        }
+    }
+);
---
+++ src/script.js
@@ -0,0 +1,5 @@
+import './scss/index.scss'
+import * as dat from 'dat.gui'
+
+console.log("-QuQ-")
+console.log(dat)
